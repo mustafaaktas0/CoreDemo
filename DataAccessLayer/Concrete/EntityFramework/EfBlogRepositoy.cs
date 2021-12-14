9b4291d1e5c89ab755961ac79315d67881314479
mustafa aktaş <mustafaaktas791@gmail.com>
2021-12-14 22:17:00 +0300

    Add project files.

---
+++ DataAccessLayer/Concrete/EntityFramework/EfBlogRepositoy.cs
@@ -0,0 +1,12 @@
+﻿using DataAccessLayer.Abstract;
+using DataAccessLayer.Repositories;
+using EntityLayer.Concrete;
+
+namespace DataAccessLayer.EntityFramework
+{
+    public class EfBlogRepositoy : GenericRepository<Blog>, IBlogDal
+    {
+    }
+
+}
+
---
+++ EntityLayer/Concrete/Writer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Writer
+    {
+        [Key]
+        public int WirterID { get; set; }
+        public string WirterName { get; set; }
+        public string WirterAbout { get; set; }
+        public string WirterImage { get; set; }
+        public string WriterMail { get; set; }
+        public string WriterPassword { get; set; }
+        public bool WirterStatus { get; set; }
+    }
+}
