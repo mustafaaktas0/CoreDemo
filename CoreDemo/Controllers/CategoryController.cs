96ad39495763523cdcf8828ab39c9419a977ccb5
mustafa aktaş <mustafaaktas791@gmail.com>
2022-01-09 14:36:22 +0300

    Tema Eklendi

---
+++ CoreDemo/Controllers/CategoryController.cs
@@ -0,0 +1,21 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Abstract;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreDemo.Controllers
+{
+    public class CategoryController : Controller
+    {
+        CategoryManager categoryManager = new CategoryManager(new EfCategoryRepositoy());
+        public IActionResult Index()
+        {
+            var values = categoryManager.GetAllList();
+            return View(values);
+        }
+    }
+}
